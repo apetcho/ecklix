ae3d904146fb75e35324c703474034d6786b2b1e
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2026-03-15 09:02:58 +0000

    Implement ekasoft::elx::Object::is_symbol() const method

--- src/elix-object.cpp
+++ src/elix-object.cpp
@@ -2138,12 +2138,16 @@ bool Object::is_number(void) const{
     return (this->is_integer() || this->is_float());
 }
 
+// -*-
+bool Object::is_symbol(void) const{
+    return this->m_type==Symbol{"Symbol"};
+}
+
 /*
 // -*-
 class Object final{
 public:
 
-bool Object::is_symbol(void) const{}
 bool Object::is_string(void) const{}
 bool Object::is_array(void) const{}
 bool Object::is_list(void) const{}
