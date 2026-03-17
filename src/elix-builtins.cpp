d0f01a909bff256cf6b3030e55b120a11ed53e80
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2026-03-17 03:39:47 +0000

    Implement ekasoft::elx::fn_bxor() function.

--- src/elix-builtins.cpp
+++ src/elix-builtins.cpp
@@ -594,8 +594,14 @@ static Object fn_bor(const Vec<Object>& args, ELix* elix){
 }
 
 static Object fn_xor(const Vec<Object>& args, ELix* elix){
-    //! @todo
-    throw ELixError(Symbol{"NotImplementedError"}, __func__);
+    // (* x ...)
+    auto pred = (args.size() >= 2);
+    ELix::validate_argc(pred, "^");
+    auto acc = args[0];
+    for(size_t i=1; i < args.size(); i++){
+        acc = acc ^ args[i];
+    }
+    return Object(acc);
 }
 
 static Object fn_lshift(const Vec<Object>& args, ELix* elix){
