0353432c30a2c6878f46eff27819aac070045c9c
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2026-03-18 09:58:43 +0000

    Implement ekasoft::elx::fn_math_erf() function.

--- src/elix-math.cpp
+++ src/elix-math.cpp
@@ -303,9 +303,16 @@ static Object fn_math_cbrt(const Vec<Object>& args, ELix* elix){
     return Object(result);
 }
 
+// -*-
 static Object fn_math_erf(const Vec<Object>& args, ELix* elix){
-    //! @todo
-    throw ELixError(Symbol{"NotImplementedError"}, __func__);
+    // (Math.erf num)
+    auto pred = (args.size()==1);
+    ELix::validate_argc(pred, "Math.erf");
+    pred = args[0].is_number();
+    ELix::validate_type(pred, "`(Math.erf num)'", "expect argument `num' to be a number.");
+    auto result = args[0].as_number().erf();
+
+    return Object(result);
 }
 
 static Object fn_math_erfc(const Vec<Object>& args, ELix* elix){
