cd346049f67c52ab21c9deab262ce201f1536beb
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2026-03-18 09:30:36 +0000

    Implement ekasoft::elx::fn_math_acos() function.

--- src/elix-math.cpp
+++ src/elix-math.cpp
@@ -118,10 +118,17 @@ static Object fn_math_asin(const Vec<Object>& args, ELix* elix){
 
 // -*-
 static Object fn_math_acos(const Vec<Object>& args, ELix* elix){
-    //! @todo
-    throw ELixError(Symbol{"NotImplementedError"}, __func__);
+    // (Math.acos num)
+    auto pred = (args.size()==1);
+    ELix::validate_argc(pred, "Math.acos");
+    pred = args[0].is_number();
+    ELix::validate_type(pred, "`(Math.acos num)'", "expect argument `num' to be a number.");
+    auto result = args[0].as_number().acos();
+
+    return Object(result);
 }
 
+// -*-
 static Object fn_math_atan(const Vec<Object>& args, ELix* elix){
     //! @todo
     throw ELixError(Symbol{"NotImplementedError"}, __func__);
