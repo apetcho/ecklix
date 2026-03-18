873fb2082d87dd3ed0426144944f0f8e97bc5267
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2026-03-18 09:39:12 +0000

    Implement ekasoft::elx::fn_math_asinh() function.

--- src/elix-math.cpp
+++ src/elix-math.cpp
@@ -192,10 +192,17 @@ static Object fn_math_tanh(const Vec<Object>& args, ELix* elix){
 
 // -*-
 static Object fn_math_asinh(const Vec<Object>& args, ELix* elix){
-    //! @todo
-    throw ELixError(Symbol{"NotImplementedError"}, __func__);
+    // (Math.asinh num)
+    auto pred = (args.size()==1);
+    ELix::validate_argc(pred, "Math.asinh");
+    pred = args[0].is_number();
+    ELix::validate_type(pred, "`(Math.asinh num)'", "expect argument `num' to be a number.");
+    auto result = args[0].as_number().asinh();
+
+    return Object(result);
 }
 
+// -*-
 static Object fn_math_acosh(const Vec<Object>& args, ELix* elix){
     //! @todo
     throw ELixError(Symbol{"NotImplementedError"}, __func__);
