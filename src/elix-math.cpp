6c5ad9aeb933a671d80d259fb84dafec5b61cb4e
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2026-03-18 10:07:08 +0000

    Implement ekasoft::elx::fn_math_log() function.

--- src/elix-math.cpp
+++ src/elix-math.cpp
@@ -386,11 +386,19 @@ static Object fn_math_isnan(const Vec<Object>& args, ELix* elix){
     return Object(result);
 }
 
+// -*-
 static Object fn_math_log(const Vec<Object>& args, ELix* elix){
-    //! @todo
-    throw ELixError(Symbol{"NotImplementedError"}, __func__);
+    // (Math.log num)
+    auto pred = (args.size()==1);
+    ELix::validate_argc(pred, "Math.log");
+    pred = args[0].is_number();
+    ELix::validate_type(pred, "`(Math.log num)'", "expect argument `num' to be a number.");
+    auto result = args[0].as_number().log();
+
+    return Object(result);
 }
 
+// -*-
 static Object fn_math_log2(const Vec<Object>& args, ELix* elix){
     //! @todo
     throw ELixError(Symbol{"NotImplementedError"}, __func__);
