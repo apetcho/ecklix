8f721be453e1d76a4333c9cc7ce8b082f0caedd4
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2026-03-18 09:37:51 +0000

    Implement ekasoft::elx::fn_math_tanhh() function.

--- src/elix-math.cpp
+++ src/elix-math.cpp
@@ -178,11 +178,19 @@ static Object fn_math_cosh(const Vec<Object>& args, ELix* elix){
     return Object(result);
 }
 
+// -*-
 static Object fn_math_tanh(const Vec<Object>& args, ELix* elix){
-    //! @todo
-    throw ELixError(Symbol{"NotImplementedError"}, __func__);
+    // (Math.tanh num)
+    auto pred = (args.size()==1);
+    ELix::validate_argc(pred, "Math.tanh");
+    pred = args[0].is_number();
+    ELix::validate_type(pred, "`(Math.tanh num)'", "expect argument `num' to be a number.");
+    auto result = args[0].as_number().tanh();
+
+    return Object(result);
 }
 
+// -*-
 static Object fn_math_asinh(const Vec<Object>& args, ELix* elix){
     //! @todo
     throw ELixError(Symbol{"NotImplementedError"}, __func__);
