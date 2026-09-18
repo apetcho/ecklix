097caeb1db018fd66f873881dae21f90d1241f74
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2026-09-18 22:00:03 +0100

    Define implementation scafold of Window structure

--- src/window.rs
+++ src/window.rs
@@ -9,3 +9,14 @@ pub struct Window {
     pub scroll_y: usize,
     pub buffer_id: Option<String>,
 }
+
+impl Window {
+    // pub fn new(...)
+    // pub fn resize(...)
+    // pub fn set_buffer(...)
+    // pub fn get_buffer_id(...)
+    // pub fn scroll_up(...)
+    // pub fn scroll_down(...)
+    // pub fn scroll_left(...)
+    // pub fn scroll_right(...)
+}
