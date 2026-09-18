cd0cc2bfba56bea04ce5c267228ee3c497efc49a
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2026-09-18 21:29:51 +0100

    Define implementation scafold of Buffer structure

--- src/buffer.rs
+++ src/buffer.rs
@@ -28,3 +28,48 @@ pub enum BufferMode {
     Visual,
     Replace,
 }
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum SyntaxMode{
+    Rust,
+    Lisp,
+    Plain,
+    Auto,
+}
+
+impl Buffer {
+    // pub fn new(...)
+    // pub fn id(..)
+    // pub fn name(...)
+    // pub fn set_name(...)
+    // pub fn content(...)
+    // pub fn position(...)
+    // pub fn position_mut(...)
+    // pub fn modified(...)
+    // pub fn set_modified(...)
+    // pub fn mode(...)
+    // pub fn set_mode(...)
+    // pub fn syntax_mode(...)
+    // pub fn set_syntax_mode(...)
+    // pub fn line_count(...)
+    // pub fn char_len(...)
+    // pub fn text_at(...)
+    // pub fn insert_char(...)
+    // pub fn delete_char(...)
+    // pub fn insert_newline(...)
+    // pub fn delete_newline(...)
+    // pub fn move_position(...)
+    // pub fn move_word(...)
+    // pub fn delete_to_line_end(...)
+    // pub fn delete_to_line_start(...)
+    // pub fn move_to_line_start(...)
+    // pub fn move_to_line_end(...)
+    // pub fn move_to_line(...)
+    // pub fn goto_positon(...)
+}
+
+/*
+impl fmt::Display for Buffer {
+    fn fmt(&self, f: mut fmt::Formatter<'_>) -> fmt::Result{}
+}
+*/
