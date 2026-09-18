bc87e1c8c1b4a5d03c07e4660a6a2c912d633547
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2026-09-18 22:15:57 +0100

    Define implementation scafold of Minubuffer structure

--- src/minibuffer.rs
+++ src/minibuffer.rs
@@ -11,3 +11,22 @@ pub struct Minibuffer {
     completion_candidates: Vec<String>,
     completion_index: usize,
 }
+
+impl Minibuffer {
+    // pub fn new(...)
+    // pub fn set_prompt(...)
+    // pub fn set_active(...)
+    // pub fn is_active(...)
+    // pub fn input(...)
+    // pub fn input_mut(...)
+    // pub fn set_message(...)
+    // pub fn message(...)
+    // pub fn set_history(...)
+    // pub fn add_to_history(...)
+    // pub fn set_completion_candidates(...)
+    // pub fn next_completion(...)
+    // pub fn prev_completion(...)
+    // pub fn apply_completion(...)
+    // pub fn show_completion(...)
+    // pub fn render(...)
+}
